67133dcd854f90411088144cbf1a243839a35758
winkyke0806-hash <winkyke0806@gmail.com>
2026-03-13 07:11:39 +0100

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AuthApp from "../hobbit-app.jsx";
+import HobbitApp from "../hobbit-tasks.jsx";
+
+function Root() {
+  const isLoggedIn = () => {
+    try { const u = JSON.parse(localStorage.getItem("hobbit_current")); return !!u?.adventureName; } catch { return false; }
+  };
+  const [loggedIn, setLoggedIn] = React.useState(isLoggedIn);
+  return loggedIn
+    ? <HobbitApp />
+    : <AuthApp onLogin={() => { setLoggedIn(true); }} />;
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Root />);
